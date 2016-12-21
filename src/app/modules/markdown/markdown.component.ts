205ad9fdf00ac830d3f16592e91d718652783de3
Ironbou <jojo-munoz1@hotmail.fr>
2016-12-21 10:34:53 +0100

    Ajout du module marked et des composant markdown

--- src/app/modules/markdown/markdown.component.ts
+++ src/app/modules/markdown/markdown.component.ts
@@ -1,7 +1,22 @@
 import { Component } from '@angular/core';
 
+import { MarkdownParserService } from './markdown.service';
+
 @Component({
   selector: 'markdown',
-  templateUrl: './markdown.component.html'
+  templateUrl: './markdown.component.html',
+  providers: [ MarkdownParserService ]
 })
 export class markdownComponent { }
+export class markdownComponent {
+  convertedText: string;
+
+  constructor(private md: MarkdownParserService) {
+
+  }
+
+  updateOutput(mdText: string) {
+    this.convertedText = this.md.convert(mdText);
+  }
+
+}
