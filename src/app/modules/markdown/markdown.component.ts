f9d120af338e6fa88b36e42b6bef107da84084d2
Ironbou <jojo-munoz1@hotmail.fr>
2016-12-21 12:04:25 +0100

    Ajout d'un css pour modifier la taille des zones de texte et un retour a la ligne forcé

--- src/app/modules/markdown/markdown.component.ts
+++ src/app/modules/markdown/markdown.component.ts
@@ -5,9 +5,9 @@ import { MarkdownParserService } from './markdown.service';
 @Component({
   selector: 'markdown',
   templateUrl: './markdown.component.html',
-  providers: [ MarkdownParserService ]
+  providers: [ MarkdownParserService ],
 })
-export class markdownComponent { }
+
 export class markdownComponent {
   convertedText: string;
 
